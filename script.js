8c6c3bd735bd01ad9c52da5452faed50929d932d
Austin Whitbeck <austinwhitbeckart@gmail.com>
2021-06-06 11:36:26 -0400

    attempt 1 at only 10

--- script.js
+++ script.js
@@ -62,6 +62,69 @@ function addPosts (data){
 }
 
 
+// function addPosts (data){
+
+
+
+//     // pieces for each card //
+//     const listOfPosts = data.data.children;
+//     console.log(listOfPosts);
+//     let tenPostsMax = 0;
+//     let i = 0;
+//     console.log(tenPostsMax);
+
+//         // only 10 loop //
+//         while (i < 10) {
+//             listOfPosts[i](data => {
+//             // console.log(data);
+//             // console.log(data.data.title);
+//               // pieces for each card //
+//             const postContainer = document.createElement('div');
+//             const postName = document.createElement('h3');
+//             const userName = document.createElement('p');
+//             const postImage = document.createElement('img');
+//             const postUrl = document.createElement('a');
+
+
+//             // container //
+//             postContainer.setAttribute('class','post-container');
+
+//             // title //
+//             postName.innerText = data.data.title;
+//             postName.setAttribute('class', 'post-title');
+//             postContainer.appendChild(postName);
+
+//             // user ID who posted //
+
+//             userName.innerText = `Posted by: ${data.data.author}`;
+//             userName.setAttribute('class', 'user-name');
+//             postContainer.appendChild(userName);
+
+
+//             // img //
+//             postImage.setAttribute('src', data.data.thumbnail);
+//             postImage.setAttribute('class', 'post-image')
+//             postContainer.appendChild(postImage);
+
+//             // url //
+//             postUrl.setAttribute('href', data.data.url);
+//             postUrl.innerText = data.data.url;
+//             postUrl.setAttribute('class', 'post-url');
+//             postContainer.appendChild(postUrl);
+
+
+//             postingSection.appendChild(postContainer);
+
+//             tenPostsMax += 1;
+//             i++;
+//             console.log(tenPostsMax);
+
+//         })
+//     }
+// }
+
+
+
 
 fetch("https://www.reddit.com/r/aww/.json").then((response) => {
     return response.json();
